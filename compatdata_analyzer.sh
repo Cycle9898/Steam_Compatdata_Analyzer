589a44e6037eda867a3abc98a71deace8b0e9b0b
Cycle9898 <cycle9898@gmail.com>
2024-10-18 19:07:20 +0200

    initial commit with the script

---
+++ compatdata_analyzer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if a path has been passed as a parameter
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <compatdata folder path>"
+    exit 1
+fi 
+
+PATH="$1"
+
+# Check path validity
+if [ ! -d "$PATH" ]; then
+    echo "Error: '$PATH' is not a valid directory."
+    exit 1
+fi
+
+# Get compatdata folders names, request Steam API and try to math them
+STEAM_GAMES=$(/usr/bin/curl https://api.steampowered.com/ISteamApps/GetAppList/v2/ 2> /dev/null)
+
+for DIR in $(/usr/bin/ls -d ${PATH}*/); do
+    DIR_NAME=$(/usr/bin/basename "$DIR")
+
+    GAME_NAME=$( /usr/bin/jq --argjson appId "$DIR_NAME" 'first(.applist | .apps | .[] | select(.appid == $appId).name)' <<< "$STEAM_GAMES" )
+
+    if [ -z "$GAME_NAME" ]
+    then
+        GAME_NAME='Unknown Game ID'
+    fi
+
+    echo "App ID: '$DIR_NAME' corresponds to the Steam game $GAME_NAME"
+done
